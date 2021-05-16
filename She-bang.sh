e0641815699208e5c2fe4c29b65ca12ae0a2b96f
leelam1710 <leelamanushreddy@gmail.com>
2021-05-16 10:07:18 +0530

    for demonstration

---
+++ She-bang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#! is She-bang which says the path of interpreter
+# it has to be in Line no 1
+# adding more she-bangs doesn't have any impact.
+
+#Comments
+
+# All the above lines which start with # are called comments. which usually used to say some amount of description of the code written.
