cede77d8c3e6c11e03d63ff9c8a01b9f8d251411
leelam1710 <leelamanushreddy@gmail.com>
2021-05-16 11:48:57 +0530

    for demonstration
    color

--- print.sh
+++ print.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "Hello World"
+#echo "Hello World"
 
 #echo Manush
 
 #echo -e "\e[1;31mWARNING!! \n THere is an error\e[0m"
 
 #echo "bye"
+
+echo "Welcome, Good Morning, Today date is 2021-02-08"
+
+echo  Welcome, Good Morning, Today date is 2021-02-08
