ffb7a878cbf0fa696cdebf4143590940e7f3a5e9
leelam1710 <leelamanushreddy@gmail.com>
2021-05-18 17:31:50 +0530

    dynamic variables testing 1 practicing variables readwrite scalarArrays Local env

--- Read.sh
+++ Read.sh
@@ -3,4 +3,5 @@
 read -rp 'Enter your name: ' name
 read -rp 'Enter your Pincode: ' Pincode
 
-echo -e "\e[31m\nYour name is $name\e[0m \e[32m\nYour Pincode is $Pincode\e[0m"
+#echo -e "\e[31m\nYour name is $name\e[0m \e[32m\nYour Pincode is $Pincode\e[0m"
+echo -e "\nYour name is $name /nYour Pincode is $Pincode"
