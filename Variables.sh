2e339e67bc2258409249a594c5df9cd46553b87f
leelam1710 <leelamanushreddy@gmail.com>
2021-05-18 17:00:44 +0530

    dynamic variables testing 1 practicing variables readwrite scalarArrays Local env

--- Variables.sh
+++ Variables.sh
@@ -7,4 +7,4 @@ NUMBER=$((8+4*8-2))
 echo $NAME
 echo "Good morning, $DATE"
 echo "Here is the date $NUMBER"
-echo "Here is the author of alchemist $AUTHOR"
+echo "Here is the author of alchemist is $AUTHOR"
