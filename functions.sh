fa55e5316681be41a17ba48ed9a22ce867a16db7
Cloud User <centos@ip-172-31-18-216.ec2.internal>
2021-05-21 11:55:25 +0000

    functions practise

---
+++ functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash/
+
+
+#way1 of declaring functions
+sample() {
+echo Hello from sample function
+}
+
+
+#way2 of declaring functions
+funtion sample1() {
+echo Hello from sample1 funciton
+}
+
+
+#accessing functions
+sample
+sample1
