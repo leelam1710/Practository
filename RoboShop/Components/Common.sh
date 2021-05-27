7bcf006bd0cc72f216b21cb6ee999a71b0bc6250
leelam1710 <leelamanushreddy@gmail.com>
2021-05-27 12:16:55 +0530

    common script with Linux Based logs

--- RoboShop/Components/Common.sh
+++ RoboShop/Components/Common.sh
@@ -5,3 +5,7 @@ if [ "$USER_ID" -ne 0 ]; then
   echo "you must be a root user to run this script"
   exit 1
 fi
+
+print(){
+  echo -e "\e[1m $(date +%c)\e[0m \e[35m$(hostname)\e[0m \e[1;36m$(COMPONENT)\e[0m :: $1"
+}
