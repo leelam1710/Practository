1276b5f8b8ab0cd07d6cc50f14210c5328ccfc42
leelam1710 <leelamanushreddy@gmail.com>
2021-05-27 12:30:15 +0530

    Cart

--- RoboShop/Components/Common.sh
+++ RoboShop/Components/Common.sh
@@ -6,6 +6,12 @@ if [ "$USER_ID" -ne 0 ]; then
   exit 1
 fi
 
-print(){
-  echo -e "\e[1m $(date +%c)\e[0m \e[35m$(hostname)\e[0m \e[1;36m$(COMPONENT)\e[0m :: $1"
+#print(){
+#  echo -e "\e[1m $(date +%c)\e[0m \e[35m$(hostname)\e[0m \e[1;36m$(COMPONENT)\e[0m :: $1"
+#}
+
+
+Print() {
+  echo -e "[\e[1;34mINFO\e[0m]----------< \e[1m S1 \e[0m>---------"
+  echo -e "[\e[1;34mINFO\e[0m]------------------------------------"
 }
