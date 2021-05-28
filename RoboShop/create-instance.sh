cdb99f0cd7274a83908f277368e4b3ffd38cfbb4
leelam1710 <leelamanushreddy@gmail.com>
2021-05-28 21:06:27 +0530

    create instance

--- RoboShop/create-instance.sh
+++ RoboShop/create-instance.sh
@@ -10,7 +10,7 @@ fi
 
 STATE=$(aws ec2 describe-instances --filters "Name=tag-value,Values=front-end" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
-if [ $STATE != "running" ]; then
+if [ "$STATE" != "running" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=lt-01ce3b1e6b2ed30db --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
   sleep 15
 fi
@@ -19,6 +19,6 @@ IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${compone
 export component
 export IPADDRESS
 
-envsubst <record.json >/tmp/${component}.json
+envsubst <record.json >/tmp/"${component}".json
 
 aws route53 change-resource-record-sets --hosted-zone-id Z00339063TJYG8SBV0FZX --change-batch file:///tmp/"${component}".json
