35e3a06835da8d9e28cd41929404de2a28e36268
leelam1710 <leelamanushreddy@gmail.com>
2021-06-02 23:11:02 +0530

    user

--- RoboShop/Components/user.sh
+++ RoboShop/Components/user.sh
@@ -27,7 +27,7 @@ Stat $?
 
 chown roboshop:roboshop /home/roboshop -R
 
-Print "Update SystemD Script for user" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.devopsb54.tk/' /home/roboshop/user/systemd.service && mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service"
+Print "Update SystemD Script for user" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.testops.ml/' /home/roboshop/user/systemd.service && mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service"
 sed -i -e 's/MONGO_ENDPOINT/mongodb-ss.testops.ml/' -e 's/REDIS_ENDPOINT/redis-ss.testops.ml/' /home/roboshop/user/systemd.service && mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 Stat $?
 
