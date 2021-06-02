ce23697e128b7c7302ff83dccdd01d143c015591
leelam1710 <leelamanushreddy@gmail.com>
2021-06-02 21:27:00 +0530

    catalogue

--- RoboShop/Components/Catalogue.sh
+++ RoboShop/Components/Catalogue.sh
@@ -22,7 +22,7 @@ rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip
 Stat $?
 
 Print "Install NOdeJS Dependencies" "npm install"
-npm install -g --unsafe-perm
+npm install --unsafe-perm
 Stat $?
 
 chown roboshop:roboshop /home/roboshop -R
